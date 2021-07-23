37b09b7f53e51588444eb798e8b625feefad1e41
Charles-504 <charles.com2010@gmail.com>
2021-07-23 20:47:33 -0300

    mudei o node modules e separei a branch para a header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,21 @@
+import "./style.scss";
+import netflixlogo from "../../assets/netflix-logo.png";
+import avatar from "../../assets/steve.png";
+import search from "../../assets/search.png";
 const Header = () => {
-    console.log("Componente Header");
+  console.log("Componente Header");
 
-    return (
-        <div>
-            <h1>Componente Header</h1>
-        </div>
-    );
+  return (
+    <div id="head">
+      <img id="logo" src={netflixlogo} />
+      <img id="search" src={search} />
+      <img id="avatar" src={avatar} />
+
+      <span>Séries</span>
+      <span>Filmes</span>
+      <span>Minha lista</span>
+    </div>
+  );
 };
 
 export { Header };
