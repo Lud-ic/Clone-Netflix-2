f775ed8448833aa94291c0b579593c38a932648a
Charles-504 <charles.com2010@gmail.com>
2021-07-24 21:09:01 -0300

    corrigindo tamanhos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,28 @@
 import "./style.scss";
- 
+
 const Header = () => {
   console.log("Componente Header");
 
   return (
     <div id="head">
-      <div class="logo">
-        <img id="logo" src="../../assets/header/netflix-logo.png" alt="logo"/>
+      <div class="icons">
+        <div class="logo">
+          <img
+            id="logo"
+            src="../../assets/header/netflix-logo.png"
+            alt="logo"
+          />
+        </div>
+        <div id="right">
+          <img id="avatar" src="../../assets/header/search.png" alt="avatar" />
+          <img id="search" src="../../assets/header/steve.png" alt="search" />
+        </div>
       </div>
-      <div id="right">
-        <img id="search" src="../../assets/header/steve.png" alt="search"/>
-        <img id="avatar" src="../../assets/header/search.png" alt="avatar"/>
-      </div>
-      <ul>
+      {/* <ul>
         <li>Séries</li>
         <li>Filmes</li>
         <li>Minha lista</li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
