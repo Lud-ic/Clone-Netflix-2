526a7619bf1bf661c3f00ce68b7a2ee4a6598c25
lud-ic <mag.ludmila@gmail.com>
2021-07-26 02:09:30 -0300

    addition of the section Trending

--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,17 +1,35 @@
 import './style.scss';
+import { fetchShows } from "../../services";
+import { useEffect, useState } from "react";
 
 const Trending = () => {
-  return (
-    <>
-  <div>
-    <p>Em alta</p>
-  </div>
-  <div>
 
-  </div>
-    </>
+  const [response, getResponse] = useState([]);
+
+  useEffect(() => {
+    const fetchAsync = async () => {
+      const data = await fetchShows();
+      getResponse(data);
+    }
+    fetchAsync();
+  }, [])
+  
+  return (
+    <div id="trending">
+      <div className="trending-text">
+        <p>Em alta</p>
+      </div>
+      <div id="trending-posters">
+        {response.map(({image}) => (
+        <img src={image.medium} alt="posters"/>
+         
+        ))}
+
+      </div>
+    </div>
+
   )
 
 }
 
-export { Trending };
+export { Trending }
