fa653a57a5d7ba12c16728df14732a67de370e91
lud-ic <mag.ludmila@gmail.com>
2021-07-27 02:13:03 -0300

    additon of the remaining time in the Description page

--- src/components/DescriptionSummary/index.js
+++ src/components/DescriptionSummary/index.js
@@ -7,10 +7,17 @@ const DescriptionSummary = ({ response }) => {
       {response && (
         <div id="summary">
           <h3>{response.name}</h3>
-          {/* <div className="time"> </div> */}
-          <p className="summary-text">{response.summary.replace("<p>","").replace("</p>", "").replace("<b>", "").replace("</b>", "")}</p>
+          <div className="time">
+            <div className="line">
+              <div className="grey-line"></div>
+              <div className="red-line"></div>
+            </div>
+
+            <div className="remaining-time">Tempo restante: 45m</div>
+          </div>
+          <p className="summary-text">{response.summary.replace("<p>", "").replace("</p>", "").replace("<b>", "").replace("</b>", "")}</p>
           <p className="info">Elenco: Jesse Plemons, Cristin Milioti, Jimmi Simpson</p>
-          <p className="info">Criação: Charlie Brooker</p>    
+          <p className="info">Criação: Charlie Brooker</p>
         </div>
       )}
     </>
